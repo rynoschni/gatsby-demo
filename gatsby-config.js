75f688d00c6060308f82d37f7462e1c52b6f540b
Ryan Schniederjan <ryno@schnietke.com>
2020-10-22 13:31:57 -0500

    First Gatsby site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ryan's Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ryan Sch`,
+      summary: `who lives and works in Houston, TX building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
